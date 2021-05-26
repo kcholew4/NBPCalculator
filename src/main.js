3b69d4f7b4f077ae13b6a7054ec9fcf65659a467
Kamil Cholewa <atevin187@gmail.com>
2021-05-26 19:45:38 +0200

    Improve visual experience

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import store from "./store";
 
 import VCalendar from "v-calendar";
 
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+
 Vue.use(VCalendar);
+Vue.component("v-select", vSelect);
 
 Vue.config.productionTip = false;
 
